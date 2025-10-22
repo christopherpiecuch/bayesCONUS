e505e8d1d4d37b3ca11d9ad2266d50d968bb55f0
christopherpiecuch <43249805+christopherpiecuch@users.noreply.github.com>
2025-10-22 13:33:45 -0400

    Update bayes_run_analyses.m
    Fixed an error. (Accidentally had commented out commands that need to be executed.)

--- bayes_run_analyses.m
+++ bayes_run_analyses.m
@@ -11,11 +11,11 @@ nameYouWant='myAnalysis'; % whatever you want to name your files
 numYouWant=6; % number of separate chains you want to run
 % 1. perform Bayesian data analysis
 % for kk=1:numYouWant % <-- if you can't/don't want to run in parallel
-%parfor kk=1:numYouWant
-%    bayes_main_code(nameYouWant,kk); 
-%end
+parfor kk=1:numYouWant
+    bayes_main_code(nameYouWant,kk); 
+end
 % 2. perform posterior prediction to get solutions on grid
-%bayes_posterior_prediction(nameYouWant,1:numYouWant)
+bayes_posterior_prediction(nameYouWant,1:numYouWant)
 % 3. make figures
 bayes_make_figures(nameYouWant,1:numYouWant)
 % 4. plot residuals
